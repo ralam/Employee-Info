113f754bc61871bdb58cbe6b4796b9294bf3d4b5
ralam <rehan.alam@gmail.com>
2015-09-28 11:23:12 -0700

    add logout button

--- Employee-Info/config/routes.rb
+++ Employee-Info/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'sessions#new'
 
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 end
