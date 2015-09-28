b6203919dd68ab17e84c88a7a61caae9046cf144
ralam <rehan.alam@gmail.com>
2015-09-28 11:07:30 -0700

    fix typos in user model

--- Employee-Info/app/models/user.rb
+++ Employee-Info/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  attr_reader :password
+  after_initialize :ensure_session_token
+
   validates :username, :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6, allow_nil: true}
 
@@ -18,7 +21,7 @@ class User < ActiveRecord::Base
   end
 
   def reset_token!
-    self.session_token = SecureRandom.urlsafe_base_64
+    self.session_token = SecureRandom.urlsafe_base64
     self.save!
     self.session_token
   end
@@ -26,6 +29,6 @@ class User < ActiveRecord::Base
   private
 
   def ensure_session_token
-    self.session_token ||= SecureRandom.urlsafe_base_64
+    self.session_token ||= SecureRandom.urlsafe_base64
   end
 end
