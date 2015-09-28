5df382a439c67a02bd4b88e4ba6501e2393a145f
ralam <rehan.alam@gmail.com>
2015-09-28 10:49:00 -0700

    build signin page

--- Employee-Info/config/routes.rb
+++ Employee-Info/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
-  resources :session, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 end
