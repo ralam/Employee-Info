3ef90e56dcf4cf98e51b8b2e942cc434e1da4652
ralam <rehan.alam@gmail.com>
2015-09-28 16:32:37 -0700

    add seed data

--- Employee-Info/app/controllers/users_controller.rb
+++ Employee-Info/app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in!(@user)
-      render :new
+      redirect_to '#index'
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
