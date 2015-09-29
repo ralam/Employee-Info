46d84a6770ee9b30e60a98c36eb6053af5336e5e
ralam <rehan.alam@gmail.com>
2015-09-29 16:46:03 -0700

    edit employee form and add submission method to view

--- Employee-Info/app/assets/javascripts/views/employees/employee_new.js
+++ Employee-Info/app/assets/javascripts/views/employees/employee_new.js
@@ -2,9 +2,31 @@ EmployeeInfo.Views.EmployeeForm = Backbone.View.extend({
   template: JST['employees/new'],
   tagName: 'form',
 
+  events: {
+    'submit' : 'submit'
+  },
+
   render: function () {
     this.$el.html(this.template({employee: this.model}))
     console.log(this.model);
     return this
+  },
+
+  submit: function(event) {
+    event.preventDefault();
+
+    var formData = $(event.currentTarget).serializeJSON();
+
+    this.model.set(projectData);
+    this.model.save(formData, {
+      success: function(employee) {
+        this.collection.add(employee);
+        Backbone.history.navigate('/index', { trigger: true })
+      }.bind(this),
+      error: function (errors, errorText) {
+        this.errors = errorText.responseJSON;
+        this.render();
+      }.bind(this)
+    });
   }
 })
