63d0a51f68b6259005911d6887c32a4fdeb87d54
ralam <rehan.alam@gmail.com>
2015-09-29 15:40:50 -0700

    allow scrolling on employee data:

--- Employee-Info/app/assets/javascripts/views/employees/employees_index.js
+++ Employee-Info/app/assets/javascripts/views/employees/employees_index.js
@@ -3,6 +3,15 @@ EmployeeInfo.Views.EmployeeIndex = Backbone.CompositeView.extend({
 
   initialize: function () {
     this.listenTo(this.collection, 'sync', this.render)
+    this.employeeWidth = $("td.employee_id").width();
+    this.birthDate = $("td.birth_date").width();
+    this.firstName = $("td.first_name").width();
+    this.lastName = $("td.last_name").width();
+    this.gender = $("td.gender").width();
+    this.hireDate = $("td.hire_date").width();
+    this.tableWidth = $("table.data").width();
+    this.windowWidth = $(window).width();
+    $(window).on("resize", this.resizeHeaders);
   },
 
   //Creates an EmployeeRow subview
@@ -13,12 +22,53 @@ EmployeeInfo.Views.EmployeeIndex = Backbone.CompositeView.extend({
     this.addSubview('#employees', view);
   },
 
+  //Resize header table cells to match data table
+  resizeHeaders: function () {
+    this.employeeWidth = $("td.employee_id").width();
+    this.birthDate = $("td.birth_date").width();
+    this.firstName = $("td.first_name").width();
+    this.lastName = $("td.last_name").width();
+    this.gender = $("td.gender").width();
+    this.hireDate = $("td.hire_date").width();
+    this.tableWidth = $("table.data").width();
+
+    //If window size is over 1300px, resize the header table as well for better experience
+    if (this.windowWidth > 1300) {
+      $("table.headers").css({
+        width: this.tableWidth
+      });
+    } else {
+      // If smaller than 1300px, let Bootstrap handle header table sizing
+      $("table.headers").removeAttr( 'style' );
+    }
+    $("th.employee_id").css({
+      width: this.employeeWidth
+    });
+    $("th.birth_date").css({
+      width: this.birthDate
+    });
+    $("th.first_name").css({
+      width: this.firstName
+    });
+    $("th.last_name").css({
+      width: this.lastName
+    });
+    $("th.gender").css({
+      width: this.gender
+    });
+    $("th.hire_date").css({
+      width: this.hireDate
+    });
+  },
+
   //Creates an EmployeeRow subview for each employee
   render: function() {
     this.$el.html(this.template());
     if (this.collection.models.length > 0) {
       this.collection.forEach(this.addEmployee.bind(this));
     }
+
+    this.resizeHeaders();
 
     return this;
   }
