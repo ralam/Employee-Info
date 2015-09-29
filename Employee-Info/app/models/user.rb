560cc528eb27252d8a62b89ca52045e44dd22e2c
ralam <rehan.alam@gmail.com>
2015-09-28 17:55:06 -0700

    Revert "Revert "add comments""
    This reverts commit 867d7ca58b136603a565810dd2dcb7a91d4f2853.

--- Employee-Info/app/models/user.rb
+++ Employee-Info/app/models/user.rb
@@ -5,21 +5,25 @@ class User < ActiveRecord::Base
   validates :username, :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6, allow_nil: true}
 
+  #finds the user by entered credentials
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return nil unless user && user.valid_password?(password)
     user
   end
 
+  #check if stored password digest is equal to the entered password
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
+  #checks if password is a valid password
   def valid_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
+  #resets users' session token
   def reset_token!
     self.session_token = SecureRandom.urlsafe_base64
     self.save!
